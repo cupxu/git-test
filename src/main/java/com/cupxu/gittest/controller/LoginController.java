6d314635208c38dd9576908cdd26a6f8c0fc2b26
cupxu <1706010425@hhu.edu.cn>
2022-05-26 15:04:34 +0800

    新增dev测试

--- src/main/java/com/cupxu/gittest/controller/LoginController.java
+++ src/main/java/com/cupxu/gittest/controller/LoginController.java
@@ -4,7 +4,9 @@ package com.cupxu.gittest.controller;
  * @description:
  * @author: cupxu
  * @create: 2022-05-26 14:32
- **/
+ */
 public class LoginController {
-
+    public void login(String username, String password) {
+        System.out.println("dev测试");
+    }
 }
