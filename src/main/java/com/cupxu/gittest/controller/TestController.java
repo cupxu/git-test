f78de954f9e61e6ef26bc652705b6bca3eda98f6
cupxu <1706010425@hhu.edu.cn>
2022-05-27 20:00:00 +0800

    编写jenkinsfile

--- src/main/java/com/cupxu/gittest/controller/TestController.java
+++ src/main/java/com/cupxu/gittest/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.cupxu.gittest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @description:
  * @author: cupxu
  * @create: 2022-05-26 14:32
  */
-public class LoginController {
-    public void login(String username, String password) {
-        System.out.println("dev测试");
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "测试成功";
     }
 }
